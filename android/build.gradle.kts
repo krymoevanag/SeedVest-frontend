ebdf0a52a3726d8f69c85ecddab01e68b6a951cd
krymoevanag <kirimievansgitonga@gmail.com>
2026-08-24 14:40:40 +0300

    Fix Firebase Android build configuration

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -29,6 +29,3 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
-plugins {
-    id("com.google.gms.google-services") version "4.4.2" apply false
-}
